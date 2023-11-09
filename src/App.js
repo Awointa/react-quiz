9628b32e6fadb25c9669f7c70449c73a127ad7b6
Awointa <akosawointa@gmail.com>
2023-11-09 13:29:07 +0100

    included a reducer store and the dataRcieved and dataFailed type of actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+	questions: [],
+	// 'loading', 'error', 'ready', 'active', 'finished'
+	status: "loading",
+};
+
+function reducer(state, action) {
+	switch (action.type) {
+		case "dataRecieved":
+			return { ...state, questions: action.payload, status: "ready" };
+		case "dataFailed":
+			return { ...state, status: "error" };
+		default:
+			throw new Error("Action Unknown");
+	}
+}
+
 export default function App() {
+	const [{ question, status }, dispatch] = useReducer(reducer, initialState);
+
 	useEffect(function () {
 		fetch("http://localhost:9000/questions")
 			.then((res) => res.json())
-			.then((data) => console.log(data).catch((err) => console.error("Error")));
+			.then((data) => dispatch({ type: "dataRecieved", payload: data }))
+			.catch((err) => dispatch({ type: "dataFailed" }));
 	}, []);
 
 	return (

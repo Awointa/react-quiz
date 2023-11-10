7db0cc8f3233abb87e52e053192eff7396c8793e
Awointa <akosawointa@gmail.com>
2023-11-10 13:18:24 +0100

    passed the answer prop to the Progress component instance

--- src/components/App.js
+++ src/components/App.js
@@ -77,6 +77,7 @@ export default function App() {
 							numQuestions={numQuestions}
 							points={points}
 							maxPossiblePoints={maxPossiblePoints}
+							answer={answer}
 						/>
 						<Question
 							question={questions[index]}

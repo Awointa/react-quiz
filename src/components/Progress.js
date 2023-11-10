5b359e122fecb559571d67fe7beddb8ebab960ac
Awointa <akosawointa@gmail.com>
2023-11-10 13:05:56 +0100

    recieved the maxPossiblePoints and added displayd it to the screen

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,10 +1,19 @@
 import React from "react";
 
-export default function Progress({ index, numQuestion }) {
+export default function Progress({
+	index,
+	numQuestions,
+	points,
+	maxPossiblePoints,
+}) {
 	return (
 		<heaader className="progress">
 			<p>
-				Question <strong>{index}</strong> / {numQuestion}
+				Question <strong>{index + 1}</strong> /{numQuestions}
+			</p>
+
+			<p>
+				<strong>{points}</strong> / {maxPossiblePoints}
 			</p>
 		</heaader>
 	);

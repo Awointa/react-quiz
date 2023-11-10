05ddd3894fc78690ab6fb914eccdf281b269a461
Awointa <akosawointa@gmail.com>
2023-11-10 13:29:40 +0100

    created the FinishedScreen component

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,8 @@
+export default function FinishScreen({ points, maxPossiblePoints }) {
+	const percentage = (points / maxPossiblePoints) * 100;
+	return (
+		<p>
+			You scored <strong>{points}</strong> out of {maxPossiblePoints}
+		</p>
+	);
+}
